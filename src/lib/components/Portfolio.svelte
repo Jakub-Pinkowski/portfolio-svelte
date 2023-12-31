42cb9121525671d5c7c5852f9553c9cf7fc95f32
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-31 17:41:56 +0100

    Update text styling in Portfolio component

--- src/lib/components/Portfolio.svelte
+++ src/lib/components/Portfolio.svelte
@@ -5,10 +5,10 @@
 	<h3 class="text-3xl uppercase text-main">Portfolio</h3>
 	<div class="my-8 flex w-full">
 		<div class="w-1/2">
-			<h4 class="text-2xl text-darkGray">Business websites</h4>
+			<h4 class="text-darkGray text-2xl">Business websites</h4>
 		</div>
 		<div class="w-1/2">
-			<h4 class="text-2xl text-darkGray">Fullstack projects</h4>
+			<h4 class="text-darkGray text-2xl">Fullstack projects</h4>
 		</div>
 	</div>
 </div>

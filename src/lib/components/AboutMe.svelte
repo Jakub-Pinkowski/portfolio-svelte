fe35c6e2198ef239ba588e315de17bd45661f648
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-31 17:23:32 +0100

    Refactor AboutMe component layout and add profile picture

--- src/lib/components/AboutMe.svelte
+++ src/lib/components/AboutMe.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
+	import me from '$lib/assets/me.jpg';
 </script>
 
-<div class="my-24 ml-8 md:ml-24">
-	<h2 class="text-3xl uppercase text-main">About Me</h2>
-	<h5 class="my-8 text-xl">Hello World!</h5>
-	<p class="my-8 text-xl">
-		I'm Jakub, a self-taught web developer with a genuine passion for programming. I have a strong
-		background in both front-end and back-end development, enabling me to create comprehensive and
-		dynamic web applications.
-	</p>
-	<p>I create and maintain websites for small business</p>
+<div class="my-24 ml-8 flex md:ml-24">
+	<div class="w-1/2 text-justify">
+		<h2 class="text-3xl uppercase text-main">About Me</h2>
+		<h5 class="my-8 text-xl">Hello World!</h5>
+		<p class="my-8 text-xl">
+			I'm Jakub, a self-taught web developer with a genuine passion for programming. I have a strong
+			background in both front-end and back-end development, enabling me to create comprehensive and
+			dynamic web applications.
+		</p>
+		<p class="my-8 text-xl">I create and maintain websites for small business</p>
+	</div>
+	<div class="w-1/2">
+		<figure>
+			<img class="rounded-lg px-8" src={me} alt="me" />
+		</figure>
+	</div>
 </div>

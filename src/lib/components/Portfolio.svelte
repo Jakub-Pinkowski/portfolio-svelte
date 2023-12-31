79ddf54d23bfb36b8cbdae242f685035968cc547
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-31 17:41:30 +0100

    Add portfolio section with business websites and fullstack projects

--- src/lib/components/Portfolio.svelte
+++ src/lib/components/Portfolio.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
 </script>
 
-<div></div>
+<div class="my-24 ml-8 md:ml-24">
+	<h3 class="text-3xl uppercase text-main">Portfolio</h3>
+	<div class="my-8 flex w-full">
+		<div class="w-1/2">
+			<h4 class="text-2xl text-darkGray">Business websites</h4>
+		</div>
+		<div class="w-1/2">
+			<h4 class="text-2xl text-darkGray">Fullstack projects</h4>
+		</div>
+	</div>
+</div>

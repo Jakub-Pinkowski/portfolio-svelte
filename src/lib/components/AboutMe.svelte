0c91a1185e5cd2e0ecce0bc1b14917ba6a685b93
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-31 17:04:19 +0100

    Update AboutMe component

--- src/lib/components/AboutMe.svelte
+++ src/lib/components/AboutMe.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 </script>
 
-<div class="my-24 ml-8">
-	<h2 class="text-2xl">About Me</h2>
+<div class="my-24 ml-8 md:ml-24">
+	<h2 class="text-3xl uppercase text-main">About Me</h2>
+	<h5 class="my-8 text-xl">Hello World!</h5>
+	<p class="my-8 text-xl">
+		I'm Jakub, a self-taught web developer with a genuine passion for programming. I have a strong
+		background in both front-end and back-end development, enabling me to create comprehensive and
+		dynamic web applications.
+	</p>
+	<p>I create and maintain websites for small business</p>
 </div>

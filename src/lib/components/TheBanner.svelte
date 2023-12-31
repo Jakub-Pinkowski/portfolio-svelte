344c19f0e65cd2b8df5c017cdc31a6f76f3cb410
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-31 16:53:29 +0100

    Update font size in app.css and TheBanner.svelte

--- src/lib/components/TheBanner.svelte
+++ src/lib/components/TheBanner.svelte
@@ -4,6 +4,6 @@
 <div class="h-screen bg-[#1a1a1d] flex items-center tracking-wide">
 	<div class="ml-24">
 		<h1 class="text-6xl text-main p-4">Jakub Pinkowski</h1>
-		<h2 class="text-2xl text-gray p-4">A self-taught Web Developer</h2>
+		<h2 class="text-3xl text-gray p-4">A self-taught Web Developer</h2>
 	</div>
 </div>

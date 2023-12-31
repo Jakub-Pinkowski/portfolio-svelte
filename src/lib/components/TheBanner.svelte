f6a99161907905685791c2f3a1f8a57dd84c504c
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-31 16:48:21 +0100

    Update fonts and styles in app.css and TheBanner.svelte

--- src/lib/components/TheBanner.svelte
+++ src/lib/components/TheBanner.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
 </script>
 
-<div class="w-screen bg-black">
-	<h1
-		class="
-        text-6xl
-    "
-	>
-		Jakub Pinkowski
-	</h1>
-	<h2>A self-taught Web Developer</h2>
+<div class="h-screen bg-[#1a1a1d] flex items-center tracking-wide">
+	<div class="ml-24">
+		<h1 class="text-6xl text-main p-4">Jakub Pinkowski</h1>
+		<h2 class="text-2xl text-gray p-4">A self-taught Web Developer</h2>
+	</div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				main: '#950740',
+				dark: '#6f2232',
+				light: '#e0aeb5',
+				gray: '#4e4e50'
+			},
+			fontFamily: {
+				custom: ['Montserrat', 'Work Sans', 'sans-serif']
+			}
+		}
 	},
 	plugins: []
 };

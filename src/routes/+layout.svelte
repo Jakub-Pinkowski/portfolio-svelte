2312da3fb961cebcc9c2a96e17dbcec5c0b2defb
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-01-03 23:16:18 +0100

    Reorder imports in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import Footer from '$lib/components/Footer.svelte';
-	import Navigation from '$lib/components/Navigation.svelte';
 	import '../app.css';
+	import Navigation from '$lib/components/Navigation.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <Navigation />

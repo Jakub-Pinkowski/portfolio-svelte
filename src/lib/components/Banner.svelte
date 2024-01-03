d625bdb1506792f3bbf7bbaa390e7211dfcdc6bb
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-01-03 22:16:29 +0100

    Add scroll down indicator to Banner component

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -6,4 +6,5 @@
 		<h1 class="p-4 text-6xl text-main">Jakub Pinkowski</h1>
 		<h2 class="p-4 text-3xl text-gray">Self-taught Web Developer</h2>
 	</div>
+	<span class="absolute bottom-2 left-2 uppercase text-light"> Scroll down </span>
 </div>

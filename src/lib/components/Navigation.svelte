cf245710ccd42e4ba36ab89c18d9bdb62d04883a
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-01-03 22:32:38 +0100

    Add navigation links and close menu functionality

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -4,6 +4,17 @@
 	import { quadOut } from 'svelte/easing';
 
 	let open: boolean = false;
+
+	const links = [
+		{ label: 'Home', path: '/' },
+		{ label: 'About Me', path: '#about-me' },
+		{ label: 'Portfolio', path: '#portfolio' },
+		{ label: 'Contact', path: '#contact' }
+	];
+
+	const closeMenu = () => {
+		open = false;
+	};
 </script>
 
 <div class="fixed right-10 top-10 z-20">
@@ -13,17 +24,41 @@
 {#if open}
 	<div class="fixed inset-x-0 top-0 z-10 bg-darkGray text-light">
 		<div class="text-center text-3xl tracking-wide">
-			{#each ['Home', 'Example', 'About', 'Contact'] as link, i}
-				<p
-					class="mx-auto w-max cursor-pointer py-4 hover:underline"
-					transition:fly={{ y: -15, delay: 50 * i }}
-				>
-					{link}
-				</p>
-			{/each}
+			<a
+				href={links[0].path}
+				class="mx-auto py-4 block cursor-pointer hover:underline"
+				on:click={closeMenu}
+				in:fly={{ y: -15, delay: 50 }}
+			>
+				{links[0].label}
+			</a>
+			<a
+				href={links[1].path}
+				class="mx-auto py-4 block cursor-pointer hover:underline"
+				on:click={closeMenu}
+				in:fly={{ y: -15, delay: 100 }}
+			>
+				{links[1].label}
+			</a>
+            <a
+                href={links[2].path}
+                class="mx-auto py-4 block cursor-pointer hover:underline"
+                on:click={closeMenu}
+                in:fly={{ y: -15, delay: 150 }}
+            >
+                {links[2].label}
+            </a>
+            <a
+                href={links[3].path}
+                class="mx-auto py-4 block cursor-pointer hover:underline"
+                on:click={closeMenu}
+                in:fly={{ y: -15, delay: 200 }}
+            >
+                {links[3].label}
+            </a>
 		</div>
 
-		<hr class=" mx-auto w-1/2" transition:scale={{ duration: 500, easing: quadOut, opacity: 1 }} />
+		<hr class=" mx-auto w-1/2" in:scale={{ duration: 500, easing: quadOut, opacity: 1 }} />
 	</div>
 {/if}
 

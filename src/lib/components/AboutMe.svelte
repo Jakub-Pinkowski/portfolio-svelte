872d8977c072061c531298453bfe01088e2911f3
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2024-01-06 17:00:48 +0100

    Update AboutMe.svelte and en.json with new descriptions

--- src/lib/components/AboutMe.svelte
+++ src/lib/components/AboutMe.svelte
@@ -10,13 +10,10 @@
 		<h3 class="text-4xl uppercase text-main">{$t('about.title')}</h3>
 		<h5 class="my-8 text-xl text-darkGray">{$t('about.subtitle')}</h5>
 		<p class="my-8 text-xl text-darkGray">
-			I'm Jakub, a self-taught web developer with a genuine passion for programming. I have a strong
-			background in both front-end and back-end development, enabling me to create comprehensive and
-			dynamic web applications.
+			{$t('about.description1')}
 		</p>
 		<p class="my-8 text-xl text-darkGray">
-			Welcome to my personal portfolio, where you can explore my projects and dedication to creating
-			high-quality software solutions.
+			{$t('about.description2')}
 		</p>
 		<!-- <p class="my-8 text-xl text-darkGray">
 			I create and maintain websites for small business. You can find my full portfolio of both

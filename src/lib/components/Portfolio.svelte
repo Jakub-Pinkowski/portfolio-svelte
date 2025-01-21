5a46aa737da6f3a738edc5d5bbde9dc4a90d8cfb
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-01-21 23:18:54 +0100

    Refactor Portfolio component styles and syntax.
    Adjusted styling by removing gradient background and updated spacing for clarity. Simplified syntax by normalizing object destructuring and component usage. These changes improve code readability and maintainability.

--- src/lib/components/Portfolio.svelte
+++ src/lib/components/Portfolio.svelte
@@ -25,12 +25,12 @@
 <div id="portfolio" class="mx-8 my-24">
     <h3 class="text-4xl uppercase text-main mb-8">Portfolio</h3>
 
-    {#each categories as { title, projects } (title)}
-        <section class="md:my-16 md:p-8 md:rounded-lg md:shadow-md md:bg-gradient-to-br md:from-lightGray md:to-gray">
+    {#each categories as {title, projects} (title)}
+        <section class="my-16 md:p-8 md:rounded-lg md:shadow-md md:bg-lightGray">
             <h4 class="text-3xl text-darkGray mb-8">{title}</h4>
             <div class="grid grid-cols-1 gap-6 md:grid-cols-3 2xl:gap-12">
                 {#each projects as project (project.name)}
-                    <ProjectCard {project} {icons} />
+                    <ProjectCard {project} {icons}/>
                 {/each}
             </div>
         </section>

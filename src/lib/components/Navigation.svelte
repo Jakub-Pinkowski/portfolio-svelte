a5739518d130ea56f35870fd6370d9c1aa94632f
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-01-24 16:47:20 +0100

    Refactor Navigation styling and transitions.
    Adjusted spacing and code formatting for consistency and readability. Enhanced the navigation menu with a smoother fly transition using `quadOut` easing for improved UI interaction.

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,72 +1,76 @@
 <script lang="ts">
-	import { Hamburger } from 'svelte-hamburgers';
-	import { fly, scale } from 'svelte/transition';
-	import { quadOut } from 'svelte/easing';
+    import {Hamburger} from 'svelte-hamburgers';
+    import {fly, scale} from 'svelte/transition';
+    import {quadOut} from 'svelte/easing';
 
-	let open: boolean = $state(false)
+    let open: boolean = $state(false)
 
-	interface Link {
-		label: string;
-		path: string;
-	}
+    interface Link {
+        label: string;
+        path: string;
+    }
 
-	const links: Link[] = [
-		{ label: 'Home', path: '/' },
-		{ label: 'About Me', path: '#about-me' },
-		{ label: 'Portfolio', path: '#portfolio' },
-		{ label: 'Contact', path: '#contact' }
-	];
+    const links: Link[] = [
+        {label: 'Home', path: '/'},
+        {label: 'About Me', path: '#about-me'},
+        {label: 'Portfolio', path: '#portfolio'},
+        {label: 'Contact', path: '#contact'}
+    ];
 
-	const closeMenu = () => {
-		open = false;
-	};
+    const closeMenu = () => {
+        open = false;
+    };
 </script>
 
 <nav class="fixed right-10 top-10 z-20">
-	<Hamburger bind:open --color="#950740" />
+    <Hamburger bind:open --color="#950740"/>
 </nav>
 
 <!--NOTE: For some unexplained reason if I use #each the flying animation doesn't work-->
 {#if open}
-	<div class="fixed inset-x-0 top-0 z-10 bg-dark-gray text-light">
-		<div class="text-center text-3xl tracking-wide">
-			<a
-				href={links[0].path}
-				class="block cursor-pointer py-4 hover:underline md:mx-96"
-				onclick={closeMenu}
-				in:fly={{ y: -15, delay: 50 }}
-			>
-				{links[0].label}
-			</a>
-			<a
-				href={links[1].path}
-				class="block cursor-pointer py-4 hover:underline md:mx-96"
-				onclick={closeMenu}
-				in:fly={{ y: -15, delay: 100 }}
-			>
-				{links[1].label}
-			</a>
-			<a
-				href={links[2].path}
-				class="block cursor-pointer py-4 hover:underline md:mx-96"
-				onclick={closeMenu}
-				in:fly={{ y: -15, delay: 150 }}
-			>
-				{links[2].label}
-			</a>
-			<a
-				href={links[3].path}
-				class="block cursor-pointer py-4 hover:underline md:mx-96"
-				onclick={closeMenu}
-				in:fly={{ y: -15, delay: 200 }}
-			>
-				{links[3].label}
-			</a>
-		</div>
+    <div
+            class="fixed inset-x-0 top-0 z-10 bg-dark-gray text-light"
+            transition:fly={{ y: -100, duration: 500, easing: quadOut }}
+    >
 
-		<hr
-			class=" mx-auto w-full md:w-1/2"
-			in:scale={{ duration: 500, easing: quadOut, opacity: 1 }}
-		/>
-	</div>
+        <div class="text-center text-3xl tracking-wide">
+            <a
+                    href={links[0].path}
+                    class="block cursor-pointer py-4 hover:underline md:mx-96"
+                    onclick={closeMenu}
+                    in:fly={{ y: -15, delay: 50 }}
+            >
+                {links[0].label}
+            </a>
+            <a
+                    href={links[1].path}
+                    class="block cursor-pointer py-4 hover:underline md:mx-96"
+                    onclick={closeMenu}
+                    in:fly={{ y: -15, delay: 100 }}
+            >
+                {links[1].label}
+            </a>
+            <a
+                    href={links[2].path}
+                    class="block cursor-pointer py-4 hover:underline md:mx-96"
+                    onclick={closeMenu}
+                    in:fly={{ y: -15, delay: 150 }}
+            >
+                {links[2].label}
+            </a>
+            <a
+                    href={links[3].path}
+                    class="block cursor-pointer py-4 hover:underline md:mx-96"
+                    onclick={closeMenu}
+                    in:fly={{ y: -15, delay: 200 }}
+            >
+                {links[3].label}
+            </a>
+        </div>
+
+        <hr
+                class=" mx-auto w-full md:w-1/2"
+                in:scale={{ duration: 500, easing: quadOut, opacity: 1 }}
+        />
+    </div>
 {/if}

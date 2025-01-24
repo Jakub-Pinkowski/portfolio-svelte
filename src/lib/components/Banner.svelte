e54fed3b9780013b1959cdd8122d1d638eb02d97
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-01-24 19:14:14 +0100

    Enhance banner text styling with bold fonts.
    Updated the "Jakub Pinkowski" and "Web Developer" text to use `font-bold` and `font-semibold` respectively. This improves visual hierarchy and ensures better emphasis on key information.

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -1,7 +1,7 @@
 <div id="banner" class="flex h-screen items-center bg-[#1a1a1d] tracking-wide">
     <div class="ml-8 md:ml-24">
-        <h1 class="p-4 text-6xl text-main">Jakub Pinkowski</h1>
-        <h2 class="p-4 text-3xl text-gray">Web Developer</h2>
+   <h1 class="p-4 text-6xl text-main font-bold">Jakub Pinkowski</h1>
+   <h2 class="p-4 text-3xl text-gray font-semibold">Web Developer</h2>
     </div>
     <span class="absolute bottom-2 left-2 uppercase text-light"> Scroll down </span>
 </div>

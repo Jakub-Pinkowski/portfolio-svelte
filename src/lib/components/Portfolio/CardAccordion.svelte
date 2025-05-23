fc44ef3eb485ae695f9130ce9fcc50886b0d2529
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-05-23 18:20:54 +0200

    Improve accessibility and functionality of CardAccordion
    Added proper ARIA attributes to enhance accessibility, ensuring better screen reader support. Replaced div-based clickable title with a label for improved usability and linked

--- src/lib/components/Portfolio/CardAccordion.svelte
+++ src/lib/components/Portfolio/CardAccordion.svelte
@@ -2,15 +2,34 @@
     import type {Project} from '$lib/types';
 
     let {project}: { project: Project } = $props();
+
+    function handleChange(event: Event) {
+        const target = event.target as HTMLInputElement;
+        target.setAttribute('aria-expanded', target.checked.toString());
+    }
 </script>
 
 <p class="grow-0 text-justify text-lg">{project.summary}</p>
 <div
         class="collapse-arrow border-light collapse rounded-lg border-t-4 border-l-4 shadow-md transition-all duration-300 lg:hover:shadow-xl"
 >
-    <input name="collapse-{project.name}" type="checkbox"/>
-    <div class="collapse-title text-dark-gray px-4 py-3 text-lg font-semibold">More info</div>
-    <div class="collapse-content flex flex-col gap-4 px-4">
+    <input 
+        id="collapse-{project.name.toLowerCase().replace(/\s+/g, '-')}" 
+        name="collapse-{project.name}" 
+        type="checkbox"
+        aria-expanded="false"
+        aria-controls="content-{project.name.toLowerCase().replace(/\s+/g, '-')}"
+        onchange={handleChange}
+    />
+    <label 
+        for="collapse-{project.name.toLowerCase().replace(/\s+/g, '-')}" 
+        class="collapse-title text-dark-gray px-4 py-3 text-lg font-semibold cursor-pointer"
+    >
+        More info
+    </label>
+    <div
+        class="collapse-content flex flex-col gap-4 px-4"
+    >
         <h3 class="border-t-2 border-t-gray-300 pt-2 text-lg font-semibold">Description:</h3>
         <p class="text-justify text-base">{project.description}</p>
         {#if project.features}

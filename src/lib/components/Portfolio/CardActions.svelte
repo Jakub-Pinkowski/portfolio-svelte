4ce628474738732bf3dbd123368c564761292655
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2025-05-23 18:19:03 +0200

    Enhance accessibility for action links in Portfolio cards
    Added descriptive `aria-label` attributes to improve screen reader accessibility for "View Code" and "Visit Website" links. Updated `alt` text for related images to provide clearer context for assistive technologies.

--- src/lib/components/Portfolio/CardActions.svelte
+++ src/lib/components/Portfolio/CardActions.svelte
@@ -7,13 +7,23 @@
 </script>
 
 <div class="card-actions mt-auto w-full flex-col justify-end gap-4 lg:flex-row lg:justify-between">
-    <a class="my-button xl:text-lg" href={project.github} target="_blank">
-        <img alt="github" class="mx-2 h-8 w-8" src={github_icon}/>
+    <a 
+        class="my-button xl:text-lg"
+        href={project.github} 
+        target="_blank"
+        aria-label="View code on GitHub (opens in a new tab)"
+    >
+        <img alt="Github logo" class="mx-2 h-8 w-8" src={github_icon}/>
         View Code
     </a>
     {#if project.src}
-        <a class="my-button xl:text-lg" href={project.src} target="_blank">
-            <img class="mx-2 h-8 w-8" src={globe_icon} alt="globe"/>
+        <a 
+            class="my-button xl:text-lg"
+            href={project.src} 
+            target="_blank"
+            aria-label="Visit website (opens in a new tab)"
+        >
+            <img class="mx-2 h-8 w-8" src={globe_icon} alt="Website icon"/>
             Visit Website
         </a>
     {/if}
